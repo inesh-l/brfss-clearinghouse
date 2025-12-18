3626e64206f05334b9c3c5a770f8e918396ecf01
Inesh Loka <ineshrloka@gmail.com>
2025-12-18 18:37:36 -0500

    remove dynamic page load (not needed anyway)

--- app/pivot/page.js
+++ app/pivot/page.js
@@ -1,18 +1,7 @@
+"use client";
+
 // app/pivot/page.js
-import nextDynamic from "next/dynamic";
-
-// Tell Next not to statically generate this page (avoids running react-pivottable on the server)
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-const PivotClient = nextDynamic(() => import("./PivotClient"), {
-  ssr: false,
-  loading: () => (
-    <div className="min-h-screen flex items-center justify-center">
-      <p className="text-sm text-stone-600">Loading pivot UI…</p>
-    </div>
-  ),
-});
+import PivotClient from "./PivotClient";
 
 export default function PivotPage() {
   return <PivotClient />;
